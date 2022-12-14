07eaa033ea94ea32e70aeb311c00faa8b79b65fc
Lords <92923519+iiX0Lords@users.noreply.github.com>
2022-12-14 19:07:13 +1100

    Update admins.lua

--- admins.lua
+++ admins.lua
@@ -1,2 +1,15 @@
 
-print("Hello")
+
+local admins = {
+  "iiX0Lord",
+  "PIKACHU_19999EXP",
+  "tymew2",
+  "jack_blackisonroblox"
+}
+
+local devs = {
+  "iiX0Lord",
+  "PIKACHU_19999EXP"
+}
+
+return admins,devs
